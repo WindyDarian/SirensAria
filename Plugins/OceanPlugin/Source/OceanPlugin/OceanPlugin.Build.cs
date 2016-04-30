a3d99f609462ae42fb09b775e30fd1239e29c4c6
xueyin wan <xueyinw@seas.upenn.edu>
2016-04-30 15:02:18 -0400

    Import Ocean Plug-in

---
+++ Plugins/OceanPlugin/Source/OceanPlugin/OceanPlugin.Build.cs
@@ -0,0 +1,23 @@
+using UnrealBuildTool;
+using System.IO;
+
+public class OceanPlugin : ModuleRules
+{
+    private string ModulePath
+    {
+        get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
+    }
+
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
+
+    public OceanPlugin(TargetInfo Target)
+    {
+        PrivateIncludePaths.AddRange(new string[] { "OceanPlugin/Private" });
+       // PublicIncludePaths.AddRange(new string[] { "OceanPlugin/Public" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Engine", "Core", "CoreUObject", "InputCore", "PhysX", "APEX", "Landscape" });
+    }
+}
